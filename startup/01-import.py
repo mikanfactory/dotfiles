1dfbfa7c5923744ddc8d0af43ee5cee61b2fdcf6
mikanfactory <k952i4j14x17@gmail.com>
2018-12-31 16:57:32 +0900

    fix bug

--- startup/01-import.py
+++ startup/01-import.py
@@ -9,12 +9,10 @@ from functools import partial
 from statistics import mean, median
 
 import arrow
-import tables
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-import ada.ipynb_utils as utils
 from IPython.core.display import display, HTML
