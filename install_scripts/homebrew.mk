5a478edaf6110b5aed35b1d7052a301105385d31
mikanfactory <k952i4j14x17@gmail.com>
2022-02-13 22:33:49 +0900

    homebrew

--- install_scripts/homebrew.mk
+++ install_scripts/homebrew.mk
@@ -1,4 +1,18 @@
+PYTHON := $(shell which python3)
+
+
 .PHONY: install
+install: install/brew_formulae install/brew_cask_formulae
+
+
+.PHONY: install/*
+install/brew_formulae:
+	brew install $(shell $(PYTHON) install_scripts/lib/line_up_lists.py install_scripts/brew_formulae.txt)
+
+
+install/brew_cask_formulae:
+	brew install $(shell $(PYTHON) install_scripts/lib/line_up_lists.py install_scripts/brew_cask_formulae.txt)
+
 
 .PHONY: dump
 dump: dump/formulae dump/cask
