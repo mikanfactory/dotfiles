043522d41256611481308c0a87a3d9d360ac2488
mikanfactory <k952i4j14x17@gmail.com>
2022-02-14 11:22:25 +0900

    add starship

---
+++ install_scripts/starship.mk
@@ -0,0 +1,12 @@
+PROJECT_ROOT := $(HOME)/code/dotfiles
+DOT_CONFIG_SRC := $(PROJECT_ROOT).config
+
+
+.PHONY: install
+install: link
+
+
+.PHONY: link
+link:
+	ln -s $(DOT_CONFIG_SRC)/startship.toml $(HOME)/.config/starship.toml
+
