05b071ea53b41f8546c98d571f36df2a882ff71d
mikanfactory <k952i4j14x17@gmail.com>
2022-02-15 21:54:34 +0900

    add hyper plugin

--- .hyper.js
+++ .hyper.js
@@ -137,7 +137,12 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: ['hyperterm-hybrid', "hyper-tab-icons", "gitrocket"],
+    plugins: [
+        'hyperterm-hybrid',
+        "hyper-tab-icons",
+        "gitrocket",
+        "hyperlinks",
+    ],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
