fe48a9a2ef910986d05c5988f0246fb528886070
mikanfactory <k952i4j14x17@gmail.com>
2026-01-02 15:28:14 +0900

    updates

--- .hyper.js
+++ .hyper.js
@@ -8,7 +8,7 @@ module.exports = {
         // or `'canary'` for less polished but more frequent updates
         updateChannel: 'stable',
         // default font size in pixels for all tabs
-        fontSize: 17,
+        fontSize: 20,
         // font family with optional fallbacks
         fontFamily: '"RobotoMono Nerd Font", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
         // default font weight: 'normal' or 'bold'
